ce7195a0688fda003ce345ee231ff7fa170681bd
Wihanga Chethiya <136898067+WihangaChethiya@users.noreply.github.com>
2025-05-23 19:05:02 +0530

    Add files via upload

---
+++ MPU6050 with SD_card/MPU_6050_with_SDCard.ino
@@ -0,0 +1,533 @@
+#include <WiFi.h>              //Built-in
+#include <WebServer.h>    //https://github.com/Pedroalbuquerque/ESP32WebServer download and place in your Libraries folder
+#include <ESPmDNS.h>
+
+#include "CSS.h" //Includes headers of the web and de style file
+#include <SD.h> 
+#include <SPI.h>
+
+/* LIBRARIES SENSORS */
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+WebServer server(80);
+
+#define servername "MCserver" //Define the name to your server... 
+
+// SSID and password of Wifi connection:
+const char* ssid = "MyCircuits";
+const char* password = "12345678";
+
+#define SD_pin 15 //G16 in my case
+
+bool   SD_present = false; //Controls if the SD card is present or not
+
+String sensorName = "MPU6050"; //Sensor name
+
+String dataString = ""; //For saving data
+
+String fileName = "DATALOG.TXT"; //File name to save the data
+
+int sleepTime = 30; //Default sleep time (s), we will read the one in the configuration file..
+
+/*********  SETUP  **********/
+
+void setup(void)
+{  
+  Serial.begin(9600);
+  WiFi.softAP("MyCircuits", "12345678"); //Network and password for the access point genereted by ESP32
+  
+  //Set your preferred server name, if you use "mcserver" the address would be http://mcserver.local/
+  if (!MDNS.begin(servername)) 
+  {          
+    Serial.println(F("Error setting up MDNS responder!")); 
+    ESP.restart(); 
+  } 
+
+  Serial.print(F("Initializing SD card..."));
+  
+  //see if the card is present and can be initialised.
+  //Note: Using the ESP32 and SD_Card readers requires a 1K to 4K7 pull-up to 3v3 on the MISO line, otherwise may not work
+  if (!SD.begin(SD_pin))
+  { 
+    Serial.println(F("Card failed or not present, no SD Card data logging possible..."));
+    SD_present = false; 
+  } 
+  else
+  {
+    Serial.println(F("Card initialised... file access enabled..."));
+    SD_present = true; 
+  }
+
+  //SENSORS
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+  #endif
+
+
+  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+  // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+  // the baud timing being too misaligned with processor ticks. You must use
+  // 38400 or slower in these cases, or use some kind of external separate
+  // crystal solution for the UART timer.
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // // wait for ready
+  // Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  // while (Serial.available() && Serial.read()); // empty buffer
+  // while (!Serial.available());                 // wait for data
+  // while (Serial.available() && Serial.read()); // empty buffer again
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+      // Calibration Time: generate offsets and calibrate our MPU6050
+      mpu.CalibrateAccel(6);
+      mpu.CalibrateGyro(6);
+      mpu.PrintActiveOffsets();
+      // turn on the DMP, now that it's ready
+      Serial.println(F("Enabling DMP..."));
+      mpu.setDMPEnabled(true);
+
+      // enable Arduino interrupt detection
+      Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+      Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+      Serial.println(F(")..."));
+      attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
+
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      dmpReady = true;
+
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+  }
+
+  // configure LED for output
+  pinMode(LED_PIN, OUTPUT);
+  
+  /*********  Server Commands  **********/
+  server.on("/",         SD_dir);
+  server.on("/upload",   File_Upload);
+  server.on("/fupload",  HTTP_POST,[](){ server.send(200);}, handleFileUpload);
+
+  server.begin();
+  
+  Serial.println("HTTP server started");
+}
+
+/*********  LOOP  **********/
+
+void loop(void)
+{
+  server.handleClient(); //Listen for client connections
+
+  //1. Read configuration File, TIP: we are only including the sampling time but we can add as many configuration variables as we need
+  File dataF = SD.open("/CONFIG.TXT");
+  String row = dataF.readStringUntil('\n');
+  
+  row.replace("Sampling rate (sec): ","");
+  row.replace("\"","");
+  row.replace("\n","");
+
+  sleepTime = row.toInt();
+  Serial.println(sleepTime);//just for debuging
+  dataF.close();
+
+  //2. We get data from sensor
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+       
+
+      #ifdef OUTPUT_READABLE_YAWPITCHROLL
+          // display Euler angles in degrees
+          mpu.dmpGetQuaternion(&q, fifoBuffer);
+          mpu.dmpGetGravity(&gravity, &q);
+          mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+          dataString = "";
+          dataString += sensorName;
+          dataString += ",Quaternion container:";
+          dataString += (ypr[0] * 180/M_PI); //Celcius
+          dataString += ",";
+          dataString += (ypr[1] * 180/M_PI);//%
+          dataString += ",";
+          dataString += (ypr[2] * 180/M_PI); //Pa
+
+          Serial.println(dataString); //just for debuging
+
+
+
+
+      #endif
+
+       
+        
+        
+        
+      // blink LED to indicate activity
+      blinkState = !blinkState;
+      digitalWrite(LED_PIN, blinkState);
+  }
+
+  //3. We save data into a file, TIP: it is possible to generate a new file automatically every time we open the webserver, 
+  //and to add a row with column names when we generate the new file
+  dataF = SD.open("/DATALOG.TXT", FILE_APPEND);
+  dataF.println(dataString);
+  dataF.close();
+  
+  delay(500);
+
+}
+
+/*********  FUNCTIONS  **********/
+//Initial page of the server web, list directory and give you the chance of deleting and uploading
+void SD_dir()
+{
+  if (SD_present) 
+  {
+    //Action acording to post, dowload or delete, by MC 2022
+    if (server.args() > 0 ) //Arguments were received, ignored if there are not arguments
+    { 
+      Serial.println(server.arg(0));
+  
+      String Order = server.arg(0);
+      Serial.println(Order);
+      
+      if (Order.indexOf("download_")>=0)
+      {
+        Order.remove(0,9);
+        SD_file_download(Order);
+        Serial.println(Order);
+      }
+  
+      if ((server.arg(0)).indexOf("delete_")>=0)
+      {
+        Order.remove(0,7);
+        SD_file_delete(Order);
+        Serial.println(Order);
+      }
+    }
+
+    File root = SD.open("/");
+    if (root) {
+      root.rewindDirectory();
+      SendHTML_Header();    
+      webpage += F("<table align='center'>");
+      webpage += F("<tr><th>Name/Type</th><th style='width:20%'>Type File/Dir</th><th>File Size</th></tr>");
+      printDirectory("/",0);
+      webpage += F("</table>");
+      SendHTML_Content();
+      root.close();
+    }
+    else 
+    {
+      SendHTML_Header();
+      webpage += F("<h3>No Files Found</h3>");
+    }
+    append_page_footer();
+    SendHTML_Content();
+    SendHTML_Stop();   //Stop is needed because no content length was sent
+  } else ReportSDNotPresent();
+}
+
+//Upload a file to the SD
+void File_Upload()
+{
+  append_page_header();
+  webpage += F("<h3>Select File to Upload</h3>"); 
+  webpage += F("<FORM action='/fupload' method='post' enctype='multipart/form-data'>");
+  webpage += F("<input class='buttons' style='width:25%' type='file' name='fupload' id = 'fupload' value=''>");
+  webpage += F("<button class='buttons' style='width:10%' type='submit'>Upload File</button><br><br>");
+  webpage += F("<a href='/'>[Back]</a><br><br>");
+  append_page_footer();
+  server.send(200, "text/html",webpage);
+}
+
+//Prints the directory, it is called in void SD_dir() 
+void printDirectory(const char * dirname, uint8_t levels)
+{
+  
+  File root = SD.open(dirname);
+
+  if(!root){
+    return;
+  }
+  if(!root.isDirectory()){
+    return;
+  }
+  File file = root.openNextFile();
+
+  int i = 0;
+  while(file){
+    if (webpage.length() > 1000) {
+      SendHTML_Content();
+    }
+    if(file.isDirectory()){
+      webpage += "<tr><td>"+String(file.isDirectory()?"Dir":"File")+"</td><td>"+String(file.name())+"</td><td></td></tr>";
+      printDirectory(file.name(), levels-1);
+    }
+    else
+    {
+      webpage += "<tr><td>"+String(file.name())+"</td>";
+      webpage += "<td>"+String(file.isDirectory()?"Dir":"File")+"</td>";
+      int bytes = file.size();
+      String fsize = "";
+      if (bytes < 1024)                     fsize = String(bytes)+" B";
+      else if(bytes < (1024 * 1024))        fsize = String(bytes/1024.0,3)+" KB";
+      else if(bytes < (1024 * 1024 * 1024)) fsize = String(bytes/1024.0/1024.0,3)+" MB";
+      else                                  fsize = String(bytes/1024.0/1024.0/1024.0,3)+" GB";
+      webpage += "<td>"+fsize+"</td>";
+      webpage += "<td>";
+      webpage += F("<FORM action='/' method='post'>"); 
+      webpage += F("<button type='submit' name='download'"); 
+      webpage += F("' value='"); webpage +="download_"+String(file.name()); webpage +=F("'>Download</button>");
+      webpage += "</td>";
+      webpage += "<td>";
+      webpage += F("<FORM action='/' method='post'>"); 
+      webpage += F("<button type='submit' name='delete'"); 
+      webpage += F("' value='"); webpage +="delete_"+String(file.name()); webpage +=F("'>Delete</button>");
+      webpage += "</td>";
+      webpage += "</tr>";
+
+    }
+    file = root.openNextFile();
+    i++;
+  }
+  file.close();
+
+ 
+}
+
+//Download a file from the SD, it is called in void SD_dir()
+void SD_file_download(String filename)
+{
+  if (SD_present) 
+  { 
+    File download = SD.open("/"+filename);
+    if (download) 
+    {
+      server.sendHeader("Content-Type", "text/text");
+      server.sendHeader("Content-Disposition", "attachment; filename="+filename);
+      server.sendHeader("Connection", "close");
+      server.streamFile(download, "application/octet-stream");
+      download.close();
+    } else ReportFileNotPresent("download"); 
+  } else ReportSDNotPresent();
+}
+
+//Handles the file upload a file to the SD
+File UploadFile;
+//Upload a new file to the Filing system
+void handleFileUpload()
+{ 
+  HTTPUpload& uploadfile = server.upload(); //See https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WebServer/srcv
+                                            //For further information on 'status' structure, there are other reasons such as a failed transfer that could be used
+  if(uploadfile.status == UPLOAD_FILE_START)
+  {
+    String filename = uploadfile.filename;
+    if(!filename.startsWith("/")) filename = "/"+filename;
+    Serial.print("Upload File Name: "); Serial.println(filename);
+    SD.remove(filename);                         //Remove a previous version, otherwise data is appended the file again
+    UploadFile = SD.open(filename, FILE_WRITE);  //Open the file for writing in SD (create it, if doesn't exist)
+    filename = String();
+  }
+  else if (uploadfile.status == UPLOAD_FILE_WRITE)
+  {
+    if(UploadFile) UploadFile.write(uploadfile.buf, uploadfile.currentSize); // Write the received bytes to the file
+  } 
+  else if (uploadfile.status == UPLOAD_FILE_END)
+  {
+    if(UploadFile)          //If the file was successfully created
+    {                                    
+      UploadFile.close();   //Close the file again
+      Serial.print("Upload Size: "); Serial.println(uploadfile.totalSize);
+      webpage = "";
+      append_page_header();
+      webpage += F("<h3>File was successfully uploaded</h3>"); 
+      webpage += F("<h2>Uploaded File Name: "); webpage += uploadfile.filename+"</h2>";
+      webpage += F("<h2>File Size: "); webpage += file_size(uploadfile.totalSize) + "</h2><br><br>"; 
+      webpage += F("<a href='/'>[Back]</a><br><br>");
+      append_page_footer();
+      server.send(200,"text/html",webpage);
+    } 
+    else
+    {
+      ReportCouldNotCreateFile("upload");
+    }
+  }
+}
+
+//Delete a file from the SD, it is called in void SD_dir()
+void SD_file_delete(String filename) 
+{ 
+  if (SD_present) { 
+    SendHTML_Header();
+    File dataFile = SD.open("/"+filename, FILE_READ); //Now read data from SD Card 
+    if (dataFile)
+    {
+      if (SD.remove("/"+filename)) {
+        Serial.println(F("File deleted successfully"));
+        webpage += "<h3>File '"+filename+"' has been erased</h3>"; 
+        webpage += F("<a href='/'>[Back]</a><br><br>");
+      }
+      else
+      { 
+        webpage += F("<h3>File was not deleted - error</h3>");
+        webpage += F("<a href='/'>[Back]</a><br><br>");
+      }
+    } else ReportFileNotPresent("delete");
+    append_page_footer(); 
+    SendHTML_Content();
+    SendHTML_Stop();
+  } else ReportSDNotPresent();
+} 
+
+//SendHTML_Header
+void SendHTML_Header()
+{
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate"); 
+  server.sendHeader("Pragma", "no-cache"); 
+  server.sendHeader("Expires", "-1"); 
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN); 
+  server.send(200, "text/html", ""); //Empty content inhibits Content-length header so we have to close the socket ourselves. 
+  append_page_header();
+  server.sendContent(webpage);
+  webpage = "";
+}
+
+//SendHTML_Content
+void SendHTML_Content()
+{
+  server.sendContent(webpage);
+  webpage = "";
+}
+
+//SendHTML_Stop
+void SendHTML_Stop()
+{
+  server.sendContent("");
+  server.client().stop(); //Stop is needed because no content length was sent
+}
+
+//ReportSDNotPresent
+void ReportSDNotPresent()
+{
+  SendHTML_Header();
+  webpage += F("<h3>No SD Card present</h3>"); 
+  webpage += F("<a href='/'>[Back]</a><br><br>");
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop();
+}
+
+//ReportFileNotPresent
+void ReportFileNotPresent(String target)
+{
+  SendHTML_Header();
+  webpage += F("<h3>File does not exist</h3>"); 
+  webpage += F("<a href='/"); webpage += target + "'>[Back]</a><br><br>";
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop();
+}
+
+//ReportCouldNotCreateFile
+void ReportCouldNotCreateFile(String target)
+{
+  SendHTML_Header();
+  webpage += F("<h3>Could Not Create Uploaded File (write-protected?)</h3>"); 
+  webpage += F("<a href='/"); webpage += target + "'>[Back]</a><br><br>";
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop();
+}
+
+//File size conversion
+String file_size(int bytes)
+{
+  String fsize = "";
+  if (bytes < 1024)                 fsize = String(bytes)+" B";
+  else if(bytes < (1024*1024))      fsize = String(bytes/1024.0,3)+" KB";
+  else if(bytes < (1024*1024*1024)) fsize = String(bytes/1024.0/1024.0,3)+" MB";
+  else                              fsize = String(bytes/1024.0/1024.0/1024.0,3)+" GB";
+  return fsize;
+}
